e7299eb326161d281bea791be910b05837aed847
Nmonde <106352535+Nmonde@users.noreply.github.com>
2023-10-20 13:48:35 -0400

    First commit

---
+++ lab-answers.js
@@ -0,0 +1,15 @@
+// Easy Going:
+// Write a for loop that will log the numbers 1 through 20.
+//  The commit message should read: "Easy Going answered"
+
+for (let i = 1; i <=20; i++) {
+    console.log(i);
+}
+console.log("Easy Going answered");
+
+// Get Even:
+// Write a for loop that will log only the even numbers in 0 through 200.
+// Hint: Think about the increment expression.
+// The commit message should read: "Get Even answered"
+
+
